d68596bfcb69680dafa85c2f442e5b27676eb0f0
Arizal Akbar Zikri <akbarpn136@gmail.com>
2020-11-11 19:31:10 +0700

    terapkan routes dan middleware Pathnormalize

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,9 @@
+mod app;
+
 use std::env;
-use actix_web::{HttpServer, App, Responder, middleware, get};
+use actix_web::{HttpServer, App, middleware};
+use crate::app::routes::AppRoutes;
 
-
-#[get("/")]
-async fn app() -> impl Responder {
-    format!("Definisi awal project")
-}
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
@@ -14,9 +12,13 @@ async fn main() -> std::io::Result<()> {
 
     let host = env::var("APP_ADDRESS").expect("Env. APP_ADDRESS diperlukan");
     let server = HttpServer::new(|| {
+        let route = AppRoutes::new();
+
         App::new()
             .wrap(middleware::Logger::default())
-            .service(app)
+            .wrap(middleware::NormalizePath::default())
+            .service(route.root)
+            .service(route.v1)
     });
 
     server
