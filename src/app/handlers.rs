b19141837e05aee1178e2ebc5bd355a58957d8f4
userbaruu <fulan.gulan@gmail.com>
2020-11-12 18:15:27 +0700

    buat handler baru untuk root route

--- src/app/handlers.rs
+++ src/app/handlers.rs
@@ -2,6 +2,15 @@ use actix_web::{Responder, get, HttpResponse};
 use crate::app::dto::UmpanBalik;
 
 #[get("/")]
+pub async fn root_handler() -> impl Responder {
+    HttpResponse::Ok().json(UmpanBalik::<String> {
+        sukses: true,
+        pesan: "route utama aeromit".to_string(),
+        hasil: "Selamat datang di aplikasi Aeromit".to_string()
+    })
+}
+
+#[get("/")]
 pub async fn app_handler() -> impl Responder {
     HttpResponse::Ok().json(UmpanBalik::<String> {
         sukses: true,
