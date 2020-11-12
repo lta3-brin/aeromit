42d925601b25a5bb0768e5d3e27b921fe20ff889
syariefa <syareeefa@gmail.com>
2020-11-12 22:24:52 +0700

    membuat dokumentasi module app

--- src/app/mod.rs
+++ src/app/mod.rs
@@ -1,3 +1,20 @@
+//! # Module app Aeromit
+//!
+//! Module ini digunakan untuk kelola aplikasi utama aeromit.
+//! Semua bagian dari module app seperti route, handlers dan lainnya
+//! digunakan didalam `main.rs`.
+//!
+//! <br />
+//!
+//! # Contoh
+//!
+//! ```rust
+//! // masukkan kedalam main.rs
+//! mod app
+//!
+//! use crate::app::{...}
+//! ```
+//!
 pub mod dto;
 pub mod routes;
 mod handlers;
