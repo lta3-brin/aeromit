7944c893d86652a2be93f98125a6fd7d9f10ea21
Arizal Akbar Zikri <akbarpn136@gmail.com>
2020-11-21 15:58:15 +0700

    menambahkan module awal error untuk aplikasi

---
+++ src/app/errors.rs
@@ -0,0 +1,39 @@
+//! # Module Errors
+//!
+//! Module ini digunakan untuk mengatasi `errors` yang mungkin terjadi
+//!
+//! <br />
+//!
+//! # Contoh
+//!
+//! ```rust
+//! use crate::kegiatan::errors::{...}
+//! ```
+use std::{env, io};
+
+/// Enum AppErrors digunakan sebagai tempat untuk mengambil jenis kesalahan yang berbeda
+/// yang mungkin terjadi dalam aplikasi
+#[derive(Debug)]
+pub enum AppErrors {
+    EnvError(env::VarError),
+    MongoError(mongodb::error::Error),
+    ActixError(io::Error)
+}
+
+impl From<env::VarError> for AppErrors {
+    fn from(err: env::VarError) -> Self {
+        Self::EnvError(err)
+    }
+}
+
+impl From<mongodb::error::Error> for AppErrors {
+    fn from(err: mongodb::error::Error) -> Self {
+        Self::MongoError(err)
+    }
+}
+
+impl From<io::Error> for AppErrors {
+    fn from(err: io::Error) -> Self {
+        Self::ActixError(err)
+    }
+}
