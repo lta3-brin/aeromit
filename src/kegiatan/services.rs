72ff29592ff5a456b8cfcd5e7359cc96a7a3bd87
Arizal Akbar Zikri <akbarpn136@gmail.com>
2020-11-26 14:00:07 +0700

    menerapkan limit dan skip query document

--- src/kegiatan/services.rs
+++ src/kegiatan/services.rs
@@ -12,13 +12,14 @@
 use mongodb::{
     Database,
     options::FindOptions,
-    bson::{self, doc, Document}
+    bson::{self, doc, Document},
 };
 use actix_web::web;
 use futures::StreamExt;
+use crate::app::errors::AppErrors;
+use crate::kegiatan::dto::DocProps;
 use crate::kegiatan::models::Kegiatan;
 use crate::kegiatan::helpers::doc_to_kegiatan;
-use crate::app::errors::AppErrors;
 
 /// # Fungsi baca_kegiatan_service
 ///
@@ -28,6 +29,7 @@ use crate::app::errors::AppErrors;
 ///
 /// # Masukan
 ///
+/// * `doc_props` - properti dokumen untuk kelola limit dan skip.
 /// * `db` - mongodb Database type yang dishare melalui _application state_.
 ///
 /// <br />
@@ -35,14 +37,24 @@ use crate::app::errors::AppErrors;
 /// # Keluaran
 ///
 /// * `Result<Vec<Kegiatan>, Error>` - keluaran berupa _enum_ `Result` yang terdiri dari kumpulan
-/// `Kegiatan` dan _Struct_ `mongodb::error::Error`.
-pub async fn baca_kegiatan_service(db: web::Data<Database>)
-    -> Result<Vec<Kegiatan>, AppErrors> {
+/// `Kegiatan` dan _Enum_ `AppErrors`.
+pub async fn baca_kegiatan_service(doc_props: web::Query<DocProps>, db: web::Data<Database>)
+                                   -> Result<Vec<Kegiatan>, AppErrors> {
     let mut kegiatan: Vec<Kegiatan> = vec![];
     let collection = db.collection("kegiatan");
-    let options = FindOptions::builder().sort(doc! { "kapan": -1 }).build();
-    let mut cursor = collection.find(None, options).await?;
 
+    let doc_limit: i64;
+    if let Some(limit) = doc_props.limit {
+        doc_limit = limit;
+    } else { doc_limit = 10; }
+
+    let options = FindOptions::builder()
+        .sort(doc! { "kapan": -1 })
+        .limit(doc_limit)
+        .skip(doc_props.skip)
+        .build();
+
+    let mut cursor = collection.find(None, options).await?;
     while let Some(result) = cursor.next().await {
         match result {
             Ok(document) => {
@@ -50,7 +62,7 @@ pub async fn baca_kegiatan_service(db: web::Data<Database>)
                 let keg = doc_to_kegiatan(dok)?;
 
                 kegiatan.push(keg);
-            },
+            }
             Err(err) => eprintln!("Error: {:?}", err)
         }
     }
