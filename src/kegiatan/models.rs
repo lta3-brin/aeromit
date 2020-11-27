804b9c498ee8350d4431397ded15230180b80938
syariefa <syareeefa@gmail.com>
2020-11-27 12:54:30 +0700

    menmabahkan dokumen untuk model kegiatan

--- src/kegiatan/models.rs
+++ src/kegiatan/models.rs
@@ -1,12 +1,29 @@
+//! # Module Model
+//!
+//! Module ini digunakan sebagai _Model Database_ bagian kegiatan.
+//!
+//! <br />
+//!
+//! # Contoh
+//!
+//! ```rust
+//! use crate::kegiatan::model::{...}
+//! ```
 use serde::{Serialize, Deserialize};
 use chrono::{DateTime, Utc};
 
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Kegiatan {
-    #[serde(rename="_id")]
+    #[serde(rename = "_id")]
     pub id: String,
+
+    /// nama kegiatan
     pub nama: String,
+
+    /// kapan kegiatan diadakan
     pub kapan: DateTime<Utc>,
-    pub ruang: String
+
+    /// dimana kegiatan diadakan
+    pub ruang: String,
 }
