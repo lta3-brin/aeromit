eacb9d2b4bcbddfddada73c2be4405c728051ec2
syariefa <syareeefa@gmail.com>
2020-11-30 09:01:33 +0700

    buat module awal pengguna

---
+++ src/pengguna/mod.rs
@@ -0,0 +1,13 @@
+//! # Module pengguna Aeromit
+//!
+//! Module ini digunakan untuk kelola pengguna aeromit dan didaftarkan
+//! kedalam `main.rs`.
+//!
+//! <br />
+//!
+//! # Contoh
+//!
+//! ```rust
+//! // masukkan kedalam main.rs
+//! mod pengguna
+//! ```
