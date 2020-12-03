6a3bca66099213eaba40d686b8856e3a3be3bcc4
Arizal Akbar Zikri <akbarpn136@gmail.com>
2020-12-04 06:22:55 +0700

    pembuatan module handler pengguna dengan dir based

---
+++ src/pengguna/handlers/mod.rs
@@ -0,0 +1,17 @@
+//! # Module Handlers
+//!
+//! Module ini digunakan sebagai _Controller_ atau penggerak atau pengelola berupa
+//! fungsi-fungsi _request_/_response_ server.
+//!
+//! Fungsi-fungsi dari module handlers ini biasa digunakan didalam module `routes`.
+//!
+//! <br />
+//!
+//! # Contoh
+//!
+//! ```rust
+//! use crate::pengguna::handlers::{...}
+//! ```
+pub mod get_user;
+pub mod get_users;
+pub mod create_user;
