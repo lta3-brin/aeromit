fcbca9a53153a4db904b3ed8473b4d72a8297a78
syariefa <syareeefa@gmail.com>
2020-12-13 21:00:00 +0700

    ubah StatusCode untuk error handling ActixWeb

--- src/app/errors.rs
+++ src/app/errors.rs
@@ -131,6 +131,7 @@ impl ResponseError for AppErrors {
             AppErrors::ParseOidError(..) => StatusCode::BAD_REQUEST,
             AppErrors::ParseChronoError(..) => StatusCode::BAD_REQUEST,
             AppErrors::InputValidationError(..) => StatusCode::BAD_REQUEST,
+            AppErrors::ActixWebError(..) => StatusCode::FORBIDDEN,
             AppErrors::JwtError(..) => StatusCode::NOT_ACCEPTABLE,
             _ => StatusCode::INTERNAL_SERVER_ERROR,
         }
