45a430b804ad8c95b256d9ce96de4d1cfb2a05ca
syariefa <syareeefa@gmail.com>
2020-12-20 21:59:37 +0700

    membuat handler untuk cek pengguna

---
+++ src/pengguna/handlers/check_user.rs
@@ -0,0 +1,58 @@
+//! # Module Check User Handler
+//!
+//! Module ini digunakan untuk periksa pengguna apakah session masih valid
+//! sebagai `handlers`.
+//!
+//! <br />
+//!
+//! # Contoh
+//!
+//! ```rust
+//! use crate::pengguna::handlers::check_user::{...}
+//! ```
+use actix_session::Session;
+use actix_web::{get, HttpResponse};
+use crate::app::dto::UmpanBalik;
+use crate::app::errors::AppErrors;
+use crate::pengguna::services::check_user;
+
+
+/// # Fungsi is_ok
+///
+/// Fungsi ini untuk menampilkan _response_ umpan balik hasil periksa session pengguna
+/// saat mengunjungi _endpoint root_ `v1/pengguna/check`.
+///
+/// <br />
+///
+/// # Masukan
+///
+/// * `session` - Actix session
+///
+/// <br />
+///
+/// # Keluaran
+///
+/// * `Result<HttpResponse, AppErrors>` - keluaran berupa _enum_ `Result` yang terdiri dari kumpulan
+/// `HttpResponse` dan _Enum_ `AppErrors`.
+#[get("pengguna/check/")]
+pub async fn is_ok(session: Session) -> Result<HttpResponse, AppErrors> {
+    let checked = check_user::run(session)?;
+
+    if checked {
+        let res = UmpanBalik::new(
+            checked,
+            "Pengguna dicek",
+            ()
+        );
+
+        Ok(HttpResponse::Accepted().json(res))
+    } else {
+        let res = UmpanBalik::new(
+            checked,
+            "Pengguna dicek",
+            ()
+        );
+
+        Ok(HttpResponse::Unauthorized().json(res))
+    }
+}
