6dafe19e111d080d38d40367f9dcd404527ff214
Arizal Akbar Zikri <akbarpn136@gmail.com>
2020-12-22 10:45:56 +0700

    struktur baru ui framework

---
+++ src_ui/src/layouts/MainLayout.vue
@@ -0,0 +1,50 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header>
+      <q-toolbar class="bg-dark">
+        <q-toolbar-title>
+          AEROMIT
+        </q-toolbar-title>
+
+        <div class="q-gutter-sm">
+          <q-btn
+            flat
+            dense
+            round
+            icon="home"
+            aria-label="Home"
+          />
+
+          <q-btn
+            flat
+            dense
+            round
+            icon="info"
+            aria-label="info"
+          />
+
+          <q-btn
+            flat
+            dense
+            round
+            icon="settings"
+            aria-label="Menu"
+          />
+        </div>
+      </q-toolbar>
+    </q-header>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+export default {
+  name: 'MainLayout',
+  data () {
+    return {}
+  }
+}
+</script>
