8471080d1728f4c5ed3fe374427252cd9903b0d8
userbaruu <fulan.gulan@gmail.com>
2020-12-23 17:20:33 +0700

    membuat komponen baru MenuSettings

---
+++ src_ui/src/components/layouts/MenuSettings.vue
@@ -0,0 +1,36 @@
+<template>
+  <q-menu>
+    <q-list>
+      <q-item clickable v-close-popup @click="goToPage('profil')">
+        <q-item-section avatar>
+          <q-icon name="perm_identity" />
+        </q-item-section>
+        <q-item-section>Profil</q-item-section>
+      </q-item>
+
+      <q-separator />
+
+      <q-item clickable v-close-popup @click="goToPage('login')">
+        <q-item-section avatar>
+          <q-icon name="login" />
+        </q-item-section>
+        <q-item-section>Masuk</q-item-section>
+      </q-item>
+    </q-list>
+  </q-menu>
+</template>
+
+<script>
+import {goToPage} from "src/handlers/components/menu";
+
+
+export default {
+  name: 'MenuSettingsComponent',
+  data () {
+    return {}
+  },
+  methods: {
+    goToPage
+  }
+}
+</script>
