6649a70c41f6fc01ac376de7eb048881dbd38d5a
syariefa <syareeefa@gmail.com>
2020-12-27 22:08:14 +0700

    membuat banner Network Error

--- src_ui/src/pages/Index.vue
+++ src_ui/src/pages/Index.vue
@@ -3,8 +3,17 @@
     <div class="row q-pt-lg q-col-gutter-md"
          :class="koleksi.length === 1 ? '' : 'justify-center'"
     >
-      <div class="col" v-if="koleksi.length === 0">
-        <q-banner inline-actions class="text-white bg-info">
+      <div class="col" v-if="anyError">
+        <q-banner rounded inline-actions class="text-white bg-red">
+          {{ errorMessage }}
+          <template v-slot:avatar>
+            <q-icon name="announcement" color="white" size="md" />
+          </template>
+        </q-banner>
+      </div>
+
+      <div class="col" v-if="!anyError && koleksi.length === 0">
+        <q-banner rounded inline-actions class="text-white bg-info">
           Oops... Belum ada kegiatan yang ditemukan.
           <template v-slot:avatar>
             <q-icon name="info" color="white" size="md" />
@@ -33,13 +42,23 @@ export default {
   },
   data() {
     return {
+      anyError: false,
+      errorMessage: "",
       koleksi: []
     }
   },
   async created() {
     this.$q.loadingBar.start()
-    this.koleksi = this.fetchKegiatan(await this.$store.dispatch("kegiatan/kegiatanAction"))
-    this.$q.loadingBar.stop()
+    try {
+      this.koleksi = this.fetchKegiatan(await this.$store.dispatch("kegiatan/kegiatanAction"))
+      this.errorMessage = ""
+      this.anyError = false
+      this.$q.loadingBar.stop()
+    } catch (err) {
+      this.errorMessage = err.message
+      this.anyError = true
+      this.$q.loadingBar.stop()
+    }
   },
   methods: {
     fetchKegiatan
