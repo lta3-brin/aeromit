42d199c83d6b8f473656e6e62086f17c9c5eac42
userbaruu <fulan.gulan@gmail.com>
2020-12-30 22:08:25 +0700

    pemisahan template dengan js untuk komponen menu

---
+++ src_ui/src/handlers/components/menu/MenuSettings.js
@@ -0,0 +1,12 @@
+import {goToPage} from "src/handlers/components/menu/index";
+
+
+export default {
+  name: 'MenuSettingsComponent',
+  data () {
+    return {}
+  },
+  methods: {
+    goToPage
+  }
+}
