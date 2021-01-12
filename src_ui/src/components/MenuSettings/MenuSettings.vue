e89d8ad00c695bd4679eff27ad7636080c5329c2
userbaruu <fulang.gulan@gmail.com>
2021-01-12 16:02:15 +0700

    membuat pengkondisian menu masuk/keluar aplikasi

--- src_ui/src/components/MenuSettings/MenuSettings.vue
+++ src_ui/src/components/MenuSettings/MenuSettings.vue
@@ -10,11 +10,18 @@
 
       <q-separator />
 
-      <q-item clickable v-close-popup @click="goToPage('masuk')">
+      <q-item clickable v-close-popup @click="goToPage('masuk')" v-if="!isSignIn">
         <q-item-section avatar>
           <q-icon name="login" />
         </q-item-section>
         <q-item-section>Masuk</q-item-section>
+      </q-item>
+
+      <q-item clickable v-close-popup @click="keluar" v-else>
+        <q-item-section avatar>
+          <q-icon name="exit_to_app" />
+        </q-item-section>
+        <q-item-section>Keluar</q-item-section>
       </q-item>
     </q-list>
   </q-menu>
