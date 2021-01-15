687ccdb8567025922beb5a35b6b7e451514f3fa0
userbaruu <fulang.gulan@gmail.com>
2021-01-15 17:16:08 +0700

    Terapan lanjut melalui query parameter untuk pagination

--- src_ui/src/store/kegiatan/actions.js
+++ src_ui/src/store/kegiatan/actions.js
@@ -1,12 +1,20 @@
 import axios from "axios"
 import {Cookies} from "quasar"
 
-export async function kegiatanAction() {
-  let header = Cookies.get("_msk")
+export async function kegiatanAction(ctx, payload) {
+  const header = Cookies.get("_msk")
+  let page = 1
+
+  if (payload) {
+    page = payload.page ? payload.page : 1
+  }
 
   return axios.get(`${process.env.APP_ADDRESS}/v1/kegiatan`, {
     headers: {
       authorization: `Bearer ${header}`
+    },
+    params: {
+      skip: (page - 1) * process.env.MAX_PAGE
     }
   })
 }
